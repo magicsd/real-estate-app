60083cb9513a5041e31f01a9a68144af1175a8a8
Aleksandr Dus <aleksandr.dus@scompler.com>
2025-01-23 22:51:22 +0300

    feat: connect react to blockchain

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,40 @@
 import { Button } from '@/components/ui/button'
+import {
+  ethers,
+  AbstractProvider,
+  BrowserProvider,
+  JsonRpcSigner,
+} from 'ethers'
+import { useEffect, useState } from 'react'
+
+type ProviderState = BrowserProvider | AbstractProvider | null
 
 function App() {
+  const [provider, setProvider] = useState<ProviderState>(null)
+  const [signer, setSigner] = useState<JsonRpcSigner | null>()
+
+  const loadBlockchainData = async () => {
+    if (!window.ethereum) {
+      console.log('Metamask not installed, using default provider.')
+
+      setProvider(ethers.getDefaultProvider())
+    } else {
+      const provider = new BrowserProvider(window.ethereum)
+      setProvider(provider)
+
+      const signer = await provider.getSigner()
+
+      setSigner(signer)
+    }
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
+
+  console.log('provider', provider)
+  console.log('signer', signer)
+
   return (
     <main className="flex flex-col items-center justify-center min-h-screen space-y-20">
       <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
