eee7446aff748e5948d3117f39ec680cd24aa612
Aleksandr Dus <aleksandr.dus@scompler.com>
2025-01-25 15:05:28 +0300

    refactor: extract blockchain data load to hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,94 +1,11 @@
-import { ethers, AbstractProvider, BrowserProvider } from 'ethers'
-import { useEffect, useState } from 'react'
 import useMetamask from './use-metamask'
 import { Header } from '@/components/Header'
 import EstateCard from '@/components/EstateCard.tsx'
 
-import RealEstateABI from './abis/RealEstate.json'
-import EscrowABI from './abis/Escrow.json'
-
-import config from './config.json'
-
-type Config = Record<
-  string,
-  Record<'realEstate' | 'escrow', Record<'address', string>>
->
-
-const typedConfig: Config = config
-
-type ProviderState = BrowserProvider | AbstractProvider | null
-
-type Trait = {
-  trait_type: string
-  value: string | number
-}
-
-type Home = {
-  id: string
-  name: string
-  description: string
-  address: string
-  image: string
-  attributes: Trait[]
-}
+import useBlockchainData from './use-blockchain-data.ts'
 
 function App() {
-  const [provider, setProvider] = useState<ProviderState>(null)
-
-  const [realEstateContract, setRealEstateContract] =
-    useState<ethers.Contract>()
-
-  const [escrowContract, setEscrowContract] = useState<ethers.Contract>()
-
-  const [homes, setHomes] = useState<Home[]>([])
-
-  const loadBlockchainData = async () => {
-    let provider = null
-
-    if (!window.ethereum) {
-      console.log('Metamask not installed, using default provider.')
-
-      provider = ethers.getDefaultProvider()
-    } else {
-      provider = new BrowserProvider(window.ethereum)
-    }
-
-    setProvider(provider)
-
-    const network = await provider.getNetwork()
-
-    const contractConfig = typedConfig[network.chainId.toString()]
-
-    const realEstateContract = new ethers.Contract(
-      contractConfig.realEstate.address,
-      RealEstateABI,
-      provider,
-    )
-
-    setRealEstateContract(realEstateContract)
-
-    setEscrowContract(
-      new ethers.Contract(contractConfig.escrow.address, EscrowABI, provider),
-    )
-
-    const totalSupply = await realEstateContract.totalSupply()
-    const homes = []
-
-    for (let i = 1; i <= totalSupply; i++) {
-      const uri = await realEstateContract.tokenURI(i)
-
-      const response = await fetch(uri)
-      const home = await response.json()
-
-      homes.push(home)
-    }
-
-    setHomes(homes)
-  }
-
-  useEffect(() => {
-    loadBlockchainData()
-  }, [])
+  const { homes } = useBlockchainData()
 
   const { account, connect } = useMetamask()
 
---
+++ src/use-blockchain-data.ts
@@ -0,0 +1,93 @@
+import { useCallback, useEffect, useState } from 'react'
+import { type AbstractProvider, BrowserProvider, ethers } from 'ethers'
+
+import RealEstateABI from './abis/RealEstate.json'
+import EscrowABI from './abis/Escrow.json'
+
+import configJSON from './config.json'
+
+type Config = Record<
+  string,
+  Record<'realEstate' | 'escrow', Record<'address', string>>
+>
+
+const config: Config = configJSON
+
+type ProviderState = BrowserProvider | AbstractProvider | null
+
+type Trait = {
+  trait_type: string
+  value: string | number
+}
+
+type Home = {
+  id: string
+  name: string
+  description: string
+  address: string
+  image: string
+  attributes: Trait[]
+}
+
+const useBlockchainData = () => {
+  const [provider, setProvider] = useState<ProviderState>(null)
+
+  const [realEstateContract, setRealEstateContract] =
+    useState<ethers.Contract>()
+
+  const [escrowContract, setEscrowContract] = useState<ethers.Contract>()
+
+  const [homes, setHomes] = useState<Home[]>([])
+
+  const handleDataLoad = useCallback(async () => {
+    let provider = null
+
+    if (!window.ethereum) {
+      console.log('Metamask not installed, using default provider.')
+
+      provider = ethers.getDefaultProvider()
+    } else {
+      provider = new BrowserProvider(window.ethereum)
+    }
+
+    setProvider(provider)
+
+    const network = await provider.getNetwork()
+
+    const contractConfig = config[network.chainId.toString()]
+
+    const realEstateContract = new ethers.Contract(
+      contractConfig.realEstate.address,
+      RealEstateABI,
+      provider,
+    )
+
+    setRealEstateContract(realEstateContract)
+
+    setEscrowContract(
+      new ethers.Contract(contractConfig.escrow.address, EscrowABI, provider),
+    )
+
+    const totalSupply = await realEstateContract.totalSupply()
+    const homes = []
+
+    for (let i = 1; i <= totalSupply; i++) {
+      const uri = await realEstateContract.tokenURI(i)
+
+      const response = await fetch(uri)
+      const home = await response.json()
+
+      homes.push(home)
+    }
+
+    setHomes(homes)
+  }, [])
+
+  useEffect(() => {
+    handleDataLoad()
+  }, [handleDataLoad])
+
+  return { provider, realEstateContract, escrowContract, homes }
+}
+
+export default useBlockchainData
